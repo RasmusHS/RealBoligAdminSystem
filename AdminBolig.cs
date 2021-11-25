14f18049f4ffcbfd9e06bf4b057a9ed618a5eb43
RasmusHS <rhsc38205@edu.ucl.dk>
2021-11-25 16:58:40 +0100

    Add project files.

---
+++ AdminBolig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealBolig
+{
+    public partial class AdminBolig : Form
+    {
+        public AdminBolig()
+        {
+            InitializeComponent();
+        }
+        private void AdminBolig_Load(object sender, EventArgs e)
+        {
+            //Home frm1 = new Home();
+            //frm1.Hide();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealBolig
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            Home frm1 = new Home() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            frm1.FormBorderStyle = FormBorderStyle.None;
+            this.pContainer.Controls.Add(frm1);
+            frm1.Show();
+        }
+
+        private void btnBolig_Click(object sender, EventArgs e)
+        {
+            AdminBolig frm2 = new AdminBolig() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            frm2.FormBorderStyle = FormBorderStyle.None;
+            this.pContainer.Controls.Add(frm2);
+            frm2.Show();
+        }
+    }
+}
